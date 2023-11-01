8bd8ebe81f6d672d7410f967dc46654476d435bb
nerovkha <144420486+nerovkha@users.noreply.github.com>
2023-11-01 15:15:23 +0200

    Add Method
    Here i am adding method for print name , age and nationalites.

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,9 +1,28 @@
 
 public class Assignment {
 
+	
+	
+
+	
+		
+		
+	static void printInformation() {
+			System.out.println("Enter name of the coder " + "Nirob" +"\n" + "Liam" +"\n"+ "ilyas");
+
+			
+			
+			
+		}	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
 	}
 
 }
