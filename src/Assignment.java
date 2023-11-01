6cff07f5fe0b7fd290ad5bc1773c8adda6df96fb
OUBOUSKEN ILYAS <iliasoub15@gmail.com>
2023-11-01 15:14:23 +0200

    attributes

--- src/Assignment.java
+++ src/Assignment.java
@@ -3,7 +3,15 @@ public class Assignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
 	}
+	
+	
+	//attributes
+		public String name;
+		public int age;
+		public String nationality;
 
 }
