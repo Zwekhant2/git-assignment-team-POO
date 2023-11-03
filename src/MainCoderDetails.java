5ed440f63992319e97794327a03d025f25e2a2b3
lekhangNguyen <lekhangnguyen213@gmail.com>
2023-11-03 13:55:57 +0200

    I am adding constructor

--- src/MainCoderDetails.java
+++ src/MainCoderDetails.java
@@ -1,60 +1,59 @@
 import java.util.Scanner;
 
 public class MainCoderDetails {
-	//Ilyas is doing Attributes
-	
-	//Attributes
+	// Ilyas is doing Attributes
+
+	// Attributes
 	public String name;
 	public int age;
 	public String nationality;
-	
-	
-	//Nirob is doing Methods
-	
+
+	// Nirob is doing Methods
+
 	// Methods
 	static void printInformation() {
-		System.out.println("Enter name of the coder " + "Nirob" +"\n" + "Liam" +"\n"+ "ilyas");
+		System.out.println("Enter name of the coder " + "Nirob" + "\n" + "Liam" + "\n" + "ilyas");
 
-		
-		
-		
 	}
-	
-	
-		
 
-		
-		
-	
-	
+	// Augustine creates a constructor
 
-	
-	//Liam is doing objects
+	public MainCoderDetails(String coderName, int coderAge, String coderNationality) {
+		this.name = coderName;
+		this.age = coderAge;
+		this.nationality = coderNationality;
+
+		System.out.println("Name : " + coderName);
+		System.out.println("Age : " + coderAge);
+		System.out.println("nationality : " + coderNationality);
+
+	}
+
+	// Liam is doing objects
 	public static void main(String[] args) {
 
 		printInformation();
 
-	Scanner in=new Scanner(System.in);
-	System.out.println("Enter name");
-	String userInput= in.nextLine();
-		
-	if(userInput.equalsIgnoreCase("Nirob")) {
-		
-		 MainCoderDetails objectNirob = new  MainCoderDetails("Nirob" , 22, "Bangladeshi");
-	}
-	else if (userInput.equalsIgnoreCase("Liam")) {
-		
-		 MainCoderDetails objectLiam= new MainCoderDetails("Liam" , 20 , "Malysia");
-	}		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter name");
+		String userInput = in.nextLine();
 
-	else if (userInput.equalsIgnoreCase("Le Khang")) {
-		 MainCoderDetails objectNirob = new  MainCoderDetails("Le Khang" , 20, "Vietnam");
-	}
-	
-	else if(userInput.equalsIgnoreCase("Ilyas")) {
-		
-		 MainCoderDetails objectNirob = new  MainCoderDetails("Ilyas" , 20, "Morocco");
-		
+		if (userInput.equalsIgnoreCase("Nirob")) {
+
+			MainCoderDetails objectNirob = new MainCoderDetails("Nirob", 22, "Bangladeshi");
+		} else if (userInput.equalsIgnoreCase("Liam")) {
+
+			MainCoderDetails objectLiam = new MainCoderDetails("Liam", 20, "Malysia");
+		}
+
+		else if (userInput.equalsIgnoreCase("Le Khang")) {
+			MainCoderDetails objectNirob = new MainCoderDetails("Le Khang", 20, "Vietnam");
+		}
+
+		else if (userInput.equalsIgnoreCase("Ilyas")) {
+
+			MainCoderDetails objectNirob = new MainCoderDetails("Ilyas", 20, "Morocco");
+
 		}
 	}
 }
